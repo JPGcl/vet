090e678ea706ad47e8d0d00dfb56754e10f1f71d
JPGcl <JPGutierrezOrtega@gmail.com>
2022-07-10 23:12:37 -0400

    DB:add FKs Pet y PetHistory: add_reference

--- db/migrate/20220711030256_add_foerign_key_to_pet_history.rb
+++ db/migrate/20220711030256_add_foerign_key_to_pet_history.rb
@@ -1,5 +1,5 @@
 class AddFoerignKeyToPetHistory < ActiveRecord::Migration[7.0]
   def change
-    belongs_to :pet, index: true, foreign_key: true
+    add_reference :pet, index: true, foreign_key: true
   end
 end
