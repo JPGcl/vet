0d3df4c36fd77a2f056e6b97aa01d96b717087de
JPGcl <JPGutierrezOrtega@gmail.com>
2022-07-10 23:59:35 -0400

    DB:add FKs Pet y PetHistory: add_reference:3

--- db/migrate/20181231205454_create_pet_histories.rb
+++ db/migrate/20181231205454_create_pet_histories.rb
@@ -3,7 +3,7 @@ class CreatePetHistories < ActiveRecord::Migration[5.2]
     create_table :pet_histories do |t|
       #t.belongs_to :pet, index: true, foreign_key: true
       t.float :weight
-      t.string :heigth
+      t.float :heigth
       t.text :description
       t.integer :pet_id
 
