7379ed9ee8439f0caa3d6bf70d7f6c57ce6e2d8d
JPGcl <JPGutierrezOrtega@gmail.com>
2022-07-11 00:22:10 -0400

    Bootstrap

--- db/migrate/20220711030237_add_foerign_key_to_pet.rb
+++ db/migrate/20220711030237_add_foerign_key_to_pet.rb
@@ -2,6 +2,7 @@ class AddFoerignKeyToPet < ActiveRecord::Migration[7.0]
   def change
     #add_reference :client, foreign_key: true
     #add_reference :uploads, :user, foreign_key: true
+    
   end
   add_foreign_key "pets", "clients"
 end
