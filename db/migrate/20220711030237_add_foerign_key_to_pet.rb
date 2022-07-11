95a732c4bd64e3ace5aec6135aed71baf2e7b38d
JPGcl <JPGutierrezOrtega@gmail.com>
2022-07-10 23:18:49 -0400

    DB:add FKs Pet y PetHistory: add_reference:1

--- db/migrate/20220711030237_add_foerign_key_to_pet.rb
+++ db/migrate/20220711030237_add_foerign_key_to_pet.rb
@@ -1,5 +1,7 @@
 class AddFoerignKeyToPet < ActiveRecord::Migration[7.0]
   def change
-    add_reference :client, index: true, foreign_key: true
+    #add_reference :client, foreign_key: true
+    #add_reference :uploads, :user, foreign_key: true
   end
+  add_foreign_key "pets", "clients"
 end
--- db/migrate/20220711030256_add_foerign_key_to_pet_history.rb
+++ db/migrate/20220711030256_add_foerign_key_to_pet_history.rb
@@ -1,5 +1,6 @@
 class AddFoerignKeyToPetHistory < ActiveRecord::Migration[7.0]
   def change
-    add_reference :pet, index: true, foreign_key: true
+    #add_reference :pet, foreign_key: true
   end
+  add_foreign_key "pet_histories", "pets"
 end
